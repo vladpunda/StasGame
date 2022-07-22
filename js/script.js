1f4cdc9f95a8e7a53f33f862d9044fe7ddf4075c
vladpunda <88967791+vladpunda@users.noreply.github.com>
2022-07-23 01:21:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const startButton = document.querySelector('.game__start-button');
+const gameStartWindow = document.querySelector('.game__start');
+
+startButton.addEventListener ('click', function() {
+gameStartWindow.classList.add('game__hidden')
+
+});
